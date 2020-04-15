54326e306ce76d42903d4e96ef2040e912c84f25
klebermd13 <62228769+klebermd13@users.noreply.github.com>
2020-04-15 12:09:43 -0300

    arquivo formulario

---
+++ PHP/formulario.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title> Fomulario de incrição </title>
+    <meta nome = "author" content=""= >
+    <meta nome = "description" content="">
+    <meta nome = "viewport" content="width = device - width, inicial-scale=1">
+</head>
+
+<body>
+
+<p> FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</P>
+
+<form action="script.php" method="post">
+    <p>seu nome:<input type="text" name="nome"/><p>
+    <p>sua idade:<input type="text" neme="idade"/><p>
+    <p> <input type="submit"/></p>
+
+</form>
+
+</body>
+
+</html>
